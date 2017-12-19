5ba2bb5d29e7cbc3406cc212f7fea18ed052ca12
D.M Connolly <danne@vipresponse.nl>
2017-12-19 15:05:38 +0100

    Fix database

--- resources/views/eredivisie/teams.blade.php
+++ resources/views/eredivisie/teams.blade.php
@@ -52,7 +52,7 @@
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="#" class="big-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <div class="col-lg-3 col-xs-6">
